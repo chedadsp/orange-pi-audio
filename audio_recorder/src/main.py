74995f113009eccf2240145cba0b6b5476ae8275
chedadsp <nenad.cetic@gmail.com>
2018-12-14 09:15:52 +0100

    Some more clean up
    Introduction of virtualenv and portable requirements specification.
    Minor clean ups.

--- audio_recorder/src/main.py
+++ audio_recorder/src/main.py
@@ -42,8 +42,6 @@ if __name__ == '__main__':
         except Exception as e:
             return str(e)
 
-    hostname = "nrajic-ZBook-15-G4"
-    
     # Finding personal IP address
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
